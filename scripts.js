0735644a1493482cbbaedf56fc162d0dd2e20e17
ibtisam <iuhmirza@gmail.com>
2022-08-21 17:01:52 +0100

    Refactor code

--- scripts.js
+++ scripts.js
@@ -7,38 +7,24 @@ const addButton = document.querySelector('#addButton')
 addButton.addEventListener('mousedown', addToList)
 
 listElements = document.querySelectorAll('li')
-listElements.forEach(item => {
+listElements.forEach(strikeAndRemove)
+
+function strikeAndRemove(item) {
     item.addEventListener('mousedown', () => {
         item.classList.toggle('completed')
         item.addEventListener('mousedown', () => {
             item.remove()
         })
     })
-})
+}
 
 function addToList() {
     const listElement = document.createElement('li')
     listElement.textContent = content.value
     if(content.value!="") {
-        listElement.addEventListener('mousedown', () => {
-            listElement.classList.toggle('completed')
-            listElement.addEventListener('mousedown', () => {
-                listElement.remove()
-            })
-        })
+        strikeAndRemove(listElement)
         toDoList.appendChild(listElement)
         content.value = ""
-    }
-    if(pad.clientHeight < writing.offsetHeight + listElement.offsetHeight) {
-        addButton.removeEventListener('mousedown', addToList)
-        addButton.addEventListener('mousedown', comparePadSize)
-    }
-}
-
-function comparePadSize() {
-    if(!(pad.clientHeight < writing.offsetHeight + listElement.offsetHeight)){
-        addToList()
-        addButton.addEventListener('mousedown', addToList)
     }
 }
 
